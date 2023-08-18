704a8fc30cd3d51a30d9917c141882a0be6ed98b
crowdbotics-dev <engineering@crowdbotics.com>
2023-08-18 14:17:33 +0000

    Update models

--- backend/home/admin.py
+++ backend/home/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Scerq
+admin.site.register(Scerq)
 
 # Register your models here.
--- backend/home/models.py
+++ backend/home/models.py
@@ -1,1 +1,6 @@
+from django.conf import settings
 from django.db import models
+class Scerq(models.Model):
+    'Generated Model'
+    jjfd = models.BigIntegerField()
+    fdgdh = models.BigIntegerField()
